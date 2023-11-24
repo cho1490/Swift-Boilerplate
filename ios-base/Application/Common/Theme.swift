ce91f96d9d07eb671f81583b39549ef5208b0845
chosanghyun <chosanghyun99@gmail.com>
2023-11-24 15:56:58 +0900

    BaseTabBarController 수정, 상수를 위한 Theme 파일 생성

---
+++ ios-base/Application/Common/Theme.swift
@@ -0,0 +1,19 @@
+//
+//  AppTheme.swift
+//  ios-base
+//
+//  Created by 조상현 on 11/24/23.
+//  Copyright © 2023 ChoSangHyun. All rights reserved.
+//
+
+struct AppTheme {
+    
+    struct Color {
+        static let 
+    }
+    
+    struct Radius {
+        
+    }
+    
+}
---
+++ ios-base/Extension/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  Color.swift
+//  ios-base
+//
+//  Created by 조상현 on 11/24/23.
+//  Copyright © 2023 ChoSangHyun. All rights reserved.
+//
+ 
+import UIKit
+
+extension UIColor {
+    
+}
